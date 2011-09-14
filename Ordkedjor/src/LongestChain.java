dfa9b97e66cfed346af09bcb67597b7e6dae4ff0
Anton Holmberg <anton.holmberg@monterosa.se>
2011-09-14 14:21:06 +0200

    Removed silly comments

--- Ordkedjor/src/LongestChain.java
+++ Ordkedjor/src/LongestChain.java
@@ -4,8 +4,6 @@ class LongestChain
     private String goalWord; // slutord i breddenförstsökningen
     int wordLength;
     
-    // Fiskpudding
-    // Lakrissås
     final char [] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
 			       'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
 			       's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 
