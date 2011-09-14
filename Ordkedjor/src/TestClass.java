103b2b4e3d8e4399a10008597d8f4d0413308d5d
mraunio <magnus_raunio@hotmail.com>
2011-09-14 11:06:17 +0200

    Testing git setup

---
+++ Ordkedjor/src/TestClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * IT DOES NOTHING
+ * 
+ * @author mraunio
+ *
+ */
+public class TestClass {
+
+	public static void main(String args[]){
+		System.out.println("Här ska vi skriva massa kod");
+	}
+}
