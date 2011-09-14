513ea20d41cca544ecf0aa9236911355696ff06f
Anton Holmberg <anton.holmberg@monterosa.se>
2011-09-14 14:00:16 +0200

    lalala

--- Ordkedjor/src/LongestChain.java
+++ Ordkedjor/src/LongestChain.java
@@ -5,6 +5,7 @@ class LongestChain
     int wordLength;
     
     // Alpha beta
+    // llalaa
     final char [] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
 			       'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
 			       's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 
