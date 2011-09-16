87974af5f1555a74567cb464b0f2a45ddbbd521d
Magnus <mraunio@kth.se>
2011-09-16 19:30:07 +0200

    Added documentation to the Timer class

--- src/util/Timer.java
+++ src/util/Timer.java
@@ -1,5 +1,11 @@
 package util;
 
+/**
+ * A timer that helps measure execution time.
+ * 
+ * @author Magnus
+ *
+ */
 public class Timer {
 
 	private long time;
@@ -15,21 +21,41 @@ public class Timer {
 		stopped = false;
 	}
 	
+	/**
+	 * Starts the timer. Also works for resetting the timer.
+	 * 
+	 */
 	public void start(){
 		time = System.currentTimeMillis();
 		stop = 0;
 		pausedTime = 0;
 	}
 	
+	/**
+	 * Gets the time that has elapsed since start() was called.
+	 * @return The elapsed time since start() was called in milliseconds
+	 */
 	public long getElapsedTime(){
+		
+		if(stopped){
+			cont();
+		}
+		
 		return System.currentTimeMillis() - (time + pausedTime);
 	}
 
+	/**
+	 * Makes the timer run again, should only be called after
+	 * stop() has been called.
+	 */
 	public void cont() {
 		pausedTime += System.currentTimeMillis() - stop;
 		stopped = false;
 	}
 
+	/**
+	 * Stops the timer. To start the timer again call cont()
+	 */
 	public void stop() {
 		
 		if(!stopped){
